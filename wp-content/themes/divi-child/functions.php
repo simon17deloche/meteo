aadfd3bf4357763e7892b3dcc5c50405110ba336
Simon <simon.deloche@le-campus-numerique.fr>
2022-09-27 10:03:11 +0200

    added the new widget named Meteo Widget

--- wp-content/themes/divi-child/functions.php
+++ wp-content/themes/divi-child/functions.php
@@ -2,8 +2,13 @@
 /**
 ** activation theme
 **/
+
+require_once 'widgets/CNAlpsWeather.php';
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+register_widget(CNAlpsWeather::class);
 function theme_enqueue_styles() {
+    register_widget(CNAlpsWeather::class);
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
